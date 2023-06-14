886faf6de66c00a6ac0efc678e18f3881d37da1f
Julien Klein <44435573+iLdro@users.noreply.github.com>
2023-06-14 16:29:01 +0200

    Creating user fixing

--- dist/createUser.js
+++ dist/createUser.js
@@ -2,35 +2,25 @@ const User = require('../model/users.js');
 const bcrypt = require('bcrypt');
 
 const createUser = async (req, res) => {
-    const { firstname, name, email, password, carteVitale } = req.body;
-    const encryptedPassword = await bcrypt.hash(password, 10);
+    var { firstname, name, email, password, carteVitale } = req.body;
+    var password = await bcrypt.hash(password, 10);
+    var carteVitale = await bcrypt.hash(carteVitale, 20);
     const user = new User({
         firstname,
         name,
         email,
-        encryptedPassword,
+        password,
         carteVitale
     });
     
     try {
         const newUser = await user.save();
         res.status(201).json(newUser);
+
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
     }
 
 
-const deleteUser = async (req, res) => {
-    try {
-        await res.user.remove();
-        res.json({ message: 'User deleted' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-    }
-
-
-
 module.exports = createUser;
-module.exports = deleteUser;
